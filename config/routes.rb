fd7b8b6237ef07ace59da42aede94408d90e4738
Andrey Blinov <andreyblinov1@yandex.ru>
2016-06-17 11:35:29 +0300

    search pager

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'customers/index'
 
   devise_for :users
-  resources :customer, only: [:index]
+  resources :customers, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
